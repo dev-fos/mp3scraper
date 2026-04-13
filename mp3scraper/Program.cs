ce83298d6754cfd995248ee0e75dfc10b1635251
islam2hamy <islam2hamy@msn.com>
2026-04-13 12:30:48 +0200

    Add files via upload

--- mp3scraper/Program.cs
+++ mp3scraper/Program.cs
@@ -9,6 +9,9 @@ using System.Configuration;
 using System.Net;
 using System.ServiceModel.Syndication;
 using System.Reflection;
+using System.Xml.Linq;
+using System.Xml;
+using System.IO;
 
 namespace mp3scraper
 {
@@ -151,6 +154,7 @@ namespace mp3scraper
                     // Max items to test remotely and place in RSS
                     o = ConfigurationManager.AppSettings["maxWebRequests" + iii.ToString("d4")];
                     mp3scr.MaxWebRequestsFromObj(o);
+
 
                     // Get any existing RSS file, for merging with the latest MP3s and checking the refresh date.
                     Console.WriteLine(iii.ToString("d4"));
@@ -328,31 +332,31 @@ namespace mp3scraper
                         if ((webRequestCnt > mp3scr.MaxWebRequests) && (mp3scr.MaxWebRequests > 0))
                         {
                             Console.WriteLine("RSS item " + webRequestCnt.ToString() + " not added because it would exceed MaxWebRequests (" +
-                                mp3scr.MaxWebRequests.ToString() + "): " + mp3Addr);
+                                mp3scr.MaxWebRequests.ToString() + "): " + System.Web.HttpUtility.UrlDecode(mp3Addr, System.Text.Encoding.UTF8));
                             continue;
                         }
                         // The count so far is within the max or the mex is unlimited.
-                        Console.WriteLine("RSS item " + webRequestCnt.ToString() + ": " + mp3Addr);
+                        Console.WriteLine("RSS item " + webRequestCnt.ToString() + ": " + System.Web.HttpUtility.UrlDecode(mp3Addr, System.Text.Encoding.UTF8));
                         try
                         {
-                            SyndicationItem newSi = mp3scr.ItemForMp3(mp3Addr, mp3scr.ChannelTitle, mp3scr.GuidPrefix, mp3scr.Url, mp3scr.AllowHttps);
+                            SyndicationItem newSi = mp3scr.ItemForMp3(System.Web.HttpUtility.UrlDecode(mp3Addr, System.Text.Encoding.UTF8), mp3scr.ChannelTitle, mp3scr.GuidPrefix, mp3scr.Url, mp3scr.AllowHttps);
                             if (newSi != null)
                             {
                                 itemsToAdd.Add(newSi);
                             }
                             else
                             {
-                                Console.WriteLine("While creating RSS items, ItemForMp3 returned null for " + mp3Addr);
+                                Console.WriteLine("While creating RSS items, ItemForMp3 returned null for " + System.Web.HttpUtility.UrlDecode(mp3Addr, System.Text.Encoding.UTF8));
                             }
                         }
                         catch (WebException webEx)
                         {
-                            Console.WriteLine("While creating RSS items, ItemForMp3 returned message for " + mp3Addr +
+                            Console.WriteLine("While creating RSS items, ItemForMp3 returned message for " + System.Web.HttpUtility.UrlDecode(mp3Addr, System.Text.Encoding.UTF8) +
                                 " | " + webEx.Message + " | " + (webEx.InnerException != null ? webEx.InnerException.Message : ""));
                         }
                         catch (Exception ex)
                         {
-                            Console.WriteLine("While creating RSS items, ItemForMp3 returned message for " + mp3Addr +
+                            Console.WriteLine("While creating RSS items, ItemForMp3 returned message for " + System.Web.HttpUtility.UrlDecode(mp3Addr, System.Text.Encoding.UTF8) +
                                 " | " + ex.Message + " | " + (ex.InnerException != null ? ex.InnerException.Message : ""));
                         }
                     }
@@ -361,7 +365,7 @@ namespace mp3scraper
                     // Create the new RSS feed and add all the items
                     try
                     {
-                        sf = mp3scr.GenerateFeed(mp3scr.ChannelTitle, mp3scr.Url, "en", mp3scr.PermissionRevoked, itemsToAdd, itemsToKeep, genVal, mp3scr.ChannelNotes);
+                        sf = mp3scr.GenerateFeed(mp3scr.ChannelTitle, mp3scr.Url, "ar", mp3scr.PermissionRevoked, itemsToAdd, itemsToKeep, genVal, mp3scr.ChannelNotes);
                     }
                     catch (Exception ex)
                     {
@@ -411,6 +415,205 @@ namespace mp3scraper
                 Console.WriteLine("Main: " + ex.Message);
                 Environment.Exit(-1);
             }
+
+
+            XmlDocument doc = new XmlDocument();
+            doc.Load(mp3scr.DestFolderName + "\\" + mp3scr.DestBase);
+
+
+            /*
+            XmlElement newElement5 = doc.CreateElement("itunes", "type", "http://your");
+            newElement5.InnerText = "serial";
+
+            XmlNode parentNode5 = doc.SelectSingleNode("//channel");
+            parentNode5.AppendChild(newElement5);
+            */
+
+            XmlNodeList cElement = doc.GetElementsByTagName("channel");
+            foreach (XmlNode node in cElement)
+            {
+                node.SelectSingleNode("description").InnerText = ConfigurationManager.AppSettings["channelDescription0001"];
+            }
+
+                XmlNodeList cGeneratorElement = doc.GetElementsByTagName("generator");
+            foreach (XmlNode node in cGeneratorElement)
+            {
+
+                XmlElement newElement = doc.CreateElement("itunestype");
+                XmlElement newElement2 = doc.CreateElement("itunescomplete");
+                XmlElement newElement3 = doc.CreateElement("itunesauthor");
+                XmlElement newElement4 = doc.CreateElement("itunestitle");
+                XmlElement newElement5 = doc.CreateElement("itunesexplicit");
+                XmlElement newElement6 = doc.CreateElement("itunescategory");
+                XmlElement newElement7 = doc.CreateElement("itunescategory");
+                XmlElement newElement8 = doc.CreateElement("itunesimage");
+
+                newElement.InnerText = "Serial";
+                newElement2.InnerText = "Yes";
+                newElement3.InnerText = ConfigurationManager.AppSettings["channelAuthor0001"];
+                newElement4.InnerText = node.ParentNode.SelectSingleNode("title")?.InnerText;
+                newElement5.InnerText = "False";
+                newElement6.SetAttribute("text", "Religion & Spirituality");
+                newElement7.SetAttribute("text", "Islam");
+                newElement6.AppendChild(newElement7);
+                newElement8.SetAttribute("href", "http://www.gocek.org/podcasts/mp3scraper-logo.jpg");
+
+                node.ParentNode.InsertAfter(newElement, node);
+                node.ParentNode.InsertAfter(newElement2, node);
+                node.ParentNode.InsertAfter(newElement3, node);
+                node.ParentNode.InsertAfter(newElement4, node);
+                node.ParentNode.InsertAfter(newElement5, node);
+                node.ParentNode.InsertAfter(newElement6, node);
+                node.ParentNode.InsertAfter(newElement8, node);
+            }
+
+            XmlNodeList iElement = doc.GetElementsByTagName("item");
+            int ii = int.Parse(ConfigurationManager.AppSettings["episodesBeginning0001"]);
+            foreach (XmlNode node in iElement)
+            {
+               
+                XmlElement newElement = doc.CreateElement("itunesduration");
+                XmlElement newElement2 = doc.CreateElement("itunesepisode");
+                XmlElement newElement3 = doc.CreateElement("itunesexplicit");
+                XmlElement newElement4 = doc.CreateElement("itunestitle");
+
+                XmlElement newElement5 = doc.CreateElement("link");
+
+              //  newElement.InnerText = ;
+                newElement2.InnerText = ii.ToString();
+                newElement3.InnerText = "false";
+                newElement4.InnerText = node.SelectSingleNode("title")?.InnerText;
+                newElement5.InnerText = ConfigurationManager.AppSettings["channelUrl0001"];
+
+                XmlNode enclosure = node.SelectSingleNode("enclosure");
+                string length = enclosure.Attributes["length"].Value;
+                 //double dataSize = Double.Parse(length);
+
+                long fileSizeInBits = long.Parse(length) * 8;
+
+                // Calculate bit rate in kbps
+                //  long fileSizeInBits = dataSize * 8;
+                //  double bitRateKbps = (double)fileSizeInBits / totalSeconds / 1000;
+
+                //Console.WriteLine(dataSize);
+
+               // double seconds = (dataSize / 1024.0) / 32 ;
+
+                double seconds = fileSizeInBits / (int.Parse(ConfigurationManager.AppSettings["mp3Bitrat0001"]) * 1000);
+
+                //return TimeSpan.FromSeconds(seconds);
+
+                newElement.InnerText = seconds.ToString();
+
+                node.AppendChild(newElement);
+                node.AppendChild(newElement2);
+                node.AppendChild(newElement3);
+                node.AppendChild(newElement4);
+                node.AppendChild(newElement5);
+
+                node.SelectSingleNode("description").InnerText = ConfigurationManager.AppSettings["itemDescription0001"];
+
+                // string title = ConfigurationManager.AppSettings["channelUrl0001"];
+
+                //Console.WriteLine($"Title: {title}");
+
+                string Title = ConfigurationManager.AppSettings["Title0001"].ToString();
+
+                if (Title == "true")
+                {
+                    ii = ii-- ;
+                    node.SelectSingleNode("title").InnerText = ConfigurationManager.AppSettings["eTitle" + ii.ToString("d4")];
+                    newElement4.InnerText = ConfigurationManager.AppSettings["eTitle" + ii.ToString("d4")];
+                }
+
+               // Console.WriteLine("Title0001: "  + ii);
+
+                ii++;
+
+            }
+            /*
+            XmlNodeList childNodes = doc.GetElementsByTagName("item");
+            foreach (XmlNode child in childNodes)
+            {
+               // foreach (XmlNode subChild in child.ChildNodes)
+               // {
+                    
+                    XmlElement newElement4 = doc.CreateElement("itunestitle");
+                    newElement4.InnerText = child.SelectSingleNode("title")?.InnerText;
+                    child.AppendChild(newElement4);
+                  //  Console.WriteLine(subChild.InnerText);
+               // }
+                Console.WriteLine("ct"+child.SelectSingleNode("title")?.InnerText);
+            }
+            */
+
+
+            doc.Save(mp3scr.DestFolderName + "\\" + mp3scr.DestBase);
+
+            XDocument xdoc = XDocument.Load(mp3scr.DestFolderName + "\\" + mp3scr.DestBase);
+
+
+            // Find the specific tag (e.g., <title>)
+            var languageElement = xdoc.Descendants("language").FirstOrDefault();
+
+            var enclosureElement = xdoc.Descendants("enclosure").FirstOrDefault();
+
+            //Console.WriteLine("le" + languageElement);
+
+            /*
+            if (titleElement != titleElement)
+            {
+            // Create the new tag
+            XElement newTag = new XElement("itunestype5", "exampleType");
+
+            // Insert the new tag after the found tag
+            languageElement.AddAfterSelf(newTag);
+            Console.WriteLine("nt" + newTag);
+             }
+            */
+
+            /*
+            XNamespace ns = "http://www.example.com";
+            XElement root = new XElement(ns + "Root",
+                new XAttribute(XNamespace.Xmlns + "itunes", "http://www.example.com"),
+                new XAttribute("Remove", "Yes"),
+                new XElement(ns + "type", "Serial"),
+                new XElement(ns + "complete", "Yes"),
+                new XElement(ns + "author", "Islamic Podcast"),
+                new XElement(ns + "title", xdoc.Descendants("title").FirstOrDefault().Value),
+                new XElement(ns + "explicit" , "False"
+            ));
+            Console.WriteLine("root" + root);
+
+            //xdoc.Root.Add(root);
+
+            languageElement.AddAfterSelf(root);
+            */
+
+            var elements = xdoc.Descendants("enclosure"); // Adjust the element name as needed
+
+            foreach (var element in elements)
+            {
+                //enclosureElement.AddAfterSelf(root);
+            }
+
+
+
+            // XElement elementToRemove = xdoc.Descendants().FirstOrDefault(e => e.Attribute("Remove")?.Value == "Yes");
+
+
+
+            xdoc.Save(mp3scr.DestFolderName + "\\" + mp3scr.DestBase);
+
+            
+            string filePath = (mp3scr.DestFolderName + "\\" + mp3scr.DestBase).ToString();
+            string content = File.ReadAllText(filePath);
+            content = content.Replace(ConfigurationManager.AppSettings["url0001"], ConfigurationManager.AppSettings["channelUrl0001"]);
+            content = content.Replace("itunes", "itunes:");
+            content = content.Replace("<rss xmlns:a10=\"http://www.w3.org/2005/Atom\" version=\"2.0\">", "<rss xmlns:a10=\"http://www.w3.org/2005/Atom\" xmlns:atom=\"http://www.w3.org/2005/Atom\" xmlns:content=\"http://purl.org/rss/1.0/modules/content/\" xmlns:googleplay=\"http://www.google.com/schemas/play-podcasts/1.0\" xmlns:itunes=\"http://www.itunes.com/dtds/podcast-1.0.dtd\" xmlns:media=\"http://search.yahoo.com/mrss/\" xmlns:podcast=\"https://podcastindex.org/namespace/1.0\" version=\"2.0\">");
+            content = content.Replace("http://www.gocek.org/podcasts/mp3scraper-logo.jpg", ConfigurationManager.AppSettings["channelLogo0001"]);
+            File.WriteAllText(filePath, content);
+            
         }
     }
 }
